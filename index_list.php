2924bef0d2dc04bb114aa028846ebf1afb18cb01
tama-yui <tama618yuina@icloud.com>
2025-07-09 17:20:14 +0900

    諸々編集

--- index_list.php
+++ index_list.php
@@ -6,6 +6,7 @@
     <title>Book List Table</title>
     <link rel="stylesheet" href="style_list.css" />
 </head>
+
 <body>
 <?php include('index_header.html'); ?>
 <h1>Book List</h1>
@@ -28,41 +29,43 @@
 </form>
 
 <!-- 本のおすすめシステム -->
-{% if recommendations %}
-<h2>おすすめの本：</h2>
-<table border="1">
+<div class="recommend">
+<!-- {% if recommendations %} -->
+<h2>おすすめの本</h2>
+<table class="recommend_table">
   <tr><th>タイトル</th><th>著者</th><th>説明</th></tr>
-  {% for book in recommendations %}
+  <!-- {% for book in recommendations %} -->
     <tr>
       <td>{{ book.title }}</td>
       <td>{{ book.author }}</td>
       <td>{{ book.desc }}</td>
     </tr>
-  {% endfor %}
+  <!-- {% endfor %} -->
 </table>
-{% endif %}
+<!-- {% endif %} -->
+</div>
 
 <!-- 表示場所　-->
-    <div class="content">
-        <table class="book_table">
-            <?php
-            $filename = "books.txt";
-            if (file_exists($filename)) {
-                $lines = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-                foreach ($lines as $line) {
-                    if (preg_match("/^(.+?) - (.+)$/u", $line, $matches)) {
-                        $title = htmlspecialchars($matches[1], ENT_QUOTES, 'UTF-8');
-                        $author = htmlspecialchars($matches[2], ENT_QUOTES, 'UTF-8');
-                        echo "<tr><td>{$title}</td><td>{$author}</td></tr>";
-                    }
+<div class="content">
+    <table class="book_table">
+        <?php
+        $filename = "books.txt";
+        if (file_exists($filename)) {
+            $lines = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+            foreach ($lines as $line) {
+                if (preg_match("/^(.+?) - (.+)$/u", $line, $matches)) {
+                    $title = htmlspecialchars($matches[1], ENT_QUOTES, 'UTF-8');
+                    $author = htmlspecialchars($matches[2], ENT_QUOTES, 'UTF-8');
+                    echo "<tr class='list'><td class='showed_title'>{$title}</td><td class='showed_author'>{$author}</td></tr>";
                 }
-            } else {
-                echo "<tr><td colspan='2'>まだ本は登録されていません。</td></tr>";
             }
-            ?>
-        </table>
-    </div>
-    <?php include('index_footer.html'); ?>
-    <script src="index_list.js"></script>
+        } else {
+            echo "<tr><td colspan='2'>まだ本は登録されていません。</td></tr>";
+        }
+        ?>
+    </table>
+</div>
+<?php include('index_footer.html'); ?>
+<script src="index_list.js"></script>
 </body>
 </html>
